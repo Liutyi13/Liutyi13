f116e124f5d9777135cd6dde2e8f137561a256ba
Liutyi13 <lutyiorest7@gmail.com>
2024-03-20 22:03:40 +0200

    Refactored code structure

---
+++ controller_lu08_120000_eakv.ts
@@ -0,0 +1,15 @@
+apple
+
+const filterEvenNumbers = numbers => numbers.filter(isEven);
+orange
+
+
+if (getRandomBoolean()) { console.log("True"); } else { console.log("False"); }
+class MyClass { constructor() { this.property = getRandomString(); } }
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+
+const reverseWords = str => str.split(" ").reverse().join(" ");
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+banana - 61
+const camelCaseToSnakeCase = str => str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
